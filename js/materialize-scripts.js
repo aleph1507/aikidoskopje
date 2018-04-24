6926853f56f614ea5b119e88c7fc269c292bc2d3
aleph1507 <xrristo@gmail.com>
2018-04-24 21:15:47 +0200

    init

---
+++ js/materialize-scripts.js
@@ -0,0 +1,5 @@
+var elem = document.querySelectorAll('.sidenav');
+var instance = M.Sidenav.init(elem);
+
+var elem = document.querySelectorAll('.carousel.wia-carousel');
+var instance = M.Carousel.init(elem);
